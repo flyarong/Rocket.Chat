7359d064b82424b95ee845c45a5213c7ce19a281
Tasso Evangelista <tasso.evangelista@rocket.chat>
2019-10-18 14:19:49 -0300

    Avoid trivial state update on useReactiveValue

--- client/hooks/useReactiveValue.js
+++ client/hooks/useReactiveValue.js
@@ -1,13 +1,21 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { Tracker } from 'meteor/tracker';
 
 import { useAutorun } from './useAutorun';
 
 export const useReactiveValue = (getValue, deps = []) => {
 	const [value, setValue] = useState(() => Tracker.nonreactive(getValue));
+	const firstRunRef = useRef(true); // computation.firstRun will reset when deps change
 
 	useAutorun(() => {
-		setValue(getValue);
+		const newValue = getValue();
+
+		if (firstRunRef.current) {
+			firstRunRef.current = false;
+			return;
+		}
+
+		setValue(() => newValue);
 	}, deps);
 
 	return value;
